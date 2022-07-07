cd060b5e9612ac7b88e6b9bfb42530d3c6aab30f
JcbBrtn <42615937+JcbBrtn@users.noreply.github.com>
2022-07-07 19:06:13 -0400

    took out the agent

--- Lunar Lander.py
+++ Lunar Lander.py
@@ -10,48 +10,23 @@ observation, info = env.reset(seed=42, return_info=True)
 
 done = False
 round = 0
-total_rounds=100
-X = []
-Y = np.array([])
+total_rounds=1
 
-agent = Linear([9])
 new_obs = []
 
 while round < total_rounds:
     
-    if 0<round:
-        #Do some learning
-        a = observation.tolist()
-        a.append(act)
-        X.append(a)
-        Y = np.append(Y, np.array([reward]))
-        agent.fit(X=np.array(X), Y=Y, epochs=10)
-        observation = new_obs
-    
     act = env.action_space.sample()
-    #Get next action by taking the least most expected reward
-    to_pred = [observation.copy() for i in range(4)]
-    for i in range(4):
-        np.append(to_pred[i], i)
-        to_pred[i] = np.array(to_pred[i])
-    to_pred = np.array(to_pred) #Yeah, I know this is ugly Numpy array conversions...
-    reward_preds = agent.predict(to_pred)
-
-    max_reward = reward_preds[0]
-    for i in range(len(reward_preds[0:])):
-        if reward_preds[i] > max_reward:
-            max_reward = reward_preds[i]
-            act = i
+    #Get next action by taking the action with the most expected reward
 
     new_obs, reward, done, info = env.step(act)
     print(f' Reward: {reward} \n Done: {done} info {info}')
-    print(f'Expected Reward: {max_reward}')
     print(f'Action: {act}')
     env.render()
 
     if done:
         observation, info = env.reset(return_info=True)
         round += 1
-        X = []
+        #End of the round, do some learning!
 
 env.close()
